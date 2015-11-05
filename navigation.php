adf64e0573cde94ef8de627e531ec5be922d1a5b
ALAINF971 <alainf971@hotmail.com>
2015-11-05 18:10:34 -0500

    First Commit

---
+++ navigation.php
@@ -0,0 +1,25 @@
+<?php 
+$html = '<nav class="navbar navbar-default" role="navigation">
+               <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
+                     <span class="sr-only">Toggle navigation</span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand dropdown" href="template.php">Home</a>
+               </div>
+               <div class="collapse navbar-collapse" id="example-navbar-collapse">
+                  <ul class="nav navbar-nav">
+				  	  <li><a href="#">page1</a></li>
+                  	  <li><a href="#">page2</a></li>
+                      <li><a href="#">page3</a></li>
+					  <li><a href="#">page4</a></li>
+                      <li><a href="#">page5</a></li>
+                      <li><a href="#">page6</a></li>
+					  <li><a href="#">page7</a></li>
+                                        </ul>
+               </div>
+            </nav>';
+	echo $html;
+?>
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alainlaptop
+ * Date: 2015-11-05
+ * Time: 10:56 AM
+ */
+echo phpinfo();
