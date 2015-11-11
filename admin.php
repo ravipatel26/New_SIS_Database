8a513a0e08fa3f6867eaaf019107ab01b7640c61
ivarletap <ravikumarpatel2610@gmail.com>
2015-11-10 19:10:23 -0500

    Student form and admin page

---
+++ admin.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Comp353 Project</title>
+
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<link href="css/general.css" rel="stylesheet" type="text/css">
+<link href="css/navColor.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<div class="container-fluid">
+	<div id="body">
+        <div id="header">
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <h1></h1>
+                </div>
+            </div>
+        </div>
+    	<div id="navigation">
+            <div class="row">
+                <?php require("navigation.php"); ?>
+            </div>
+        </div>
+    	<div class="row">
+               
+        </div>
+		
+
+	</div><!---body--->
+</div><!----container fluid------->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery_min_1112.js"></script>
+    
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
