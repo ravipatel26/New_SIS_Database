492ab2c284b68009506264c90013e95ce09d27b5
Alain <alainf971@hotmail.com>
2015-11-15 11:42:53 -0500

    prepared the templates.
    Templates for the different sections.

--- index.php
+++ index.php
@@ -4,51 +4,84 @@ ini_set('display_errors', 'on');
 ini_set('log_errors', 1);
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 ?>
-<!doctype html>
-<html>
+<?php
+include("/lib/config.php");
+require("/lib/sqlQueries.php");
+require("/lib/studenFormValid.php");
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns:width="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
-<title>Comp353 Project</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-<!-- Latest compiled and minified CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <title>Comp 335</title>
 
-<link href="css/general.css" rel="stylesheet" type="text/css">
-<link href="css/navColor.css" rel="stylesheet" type="text/css">
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/basic-template.css" rel="stylesheet" />
+
+
+
+    <link href="css/general.css" rel="stylesheet" type="text/css">
+    <link href="css/navColor.css" rel="stylesheet" type="text/css">
+    <!--    <link href="../css/datepicker.css" rel="stylesheet" type="text/css">-->
+
+    <!-- BootstrapValidator CSS -->
+    <link href="css/bootstrapValidator.min.css" rel="stylesheet"/>
+
+
+    <link href="css/general.css" rel="stylesheet" type="text/css">
+    <link href="css/navColor.css" rel="stylesheet" type="text/css">
+
+    <!-- jQuery and Bootstrap JS -->
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+
+
+    <!-- BootstrapValidator -->
+    <script src="js/bootstrapValidator.min.js" type="text/javascript"></script>
+
+    <!-- Include Bootstrap Datepicker -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css" />
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/formValidation.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="js/html5shiv.min.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->
+    <script src="js/control.js"></script>
+
 </head>
-
 <body>
-<div class="container-fluid">
-	<div id="body">
-        <div id="header">
-            <div class="row">
-                <div class="col-xs-12 text-center">
-                    <h1></h1>
-                </div>
-            </div>
+<div class="container-fluid bg-info">
+    <div id="navigation">
+        <div class="row">
+            <?php require("navigation.php"); ?>
         </div>
-    	<div id="navigation">
-            <div class="row">
-                <?php require("navigation.php"); ?>
-            </div>
-        </div>
-    	<div class="row">
-               
-        </div>
-		
+    </div>
+    <div class="panel panel-default  col-lg-6 col-lg-offset-1" style="width: 80%; height: 120%">
+        <div class="panel-heading h2 text-center">Professor Information Form</div>
+        <div class="panel-body">
 
-	</div><!---body--->
-</div><!----container fluid------->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery_min_1112.js"></script>
-    
-    <script src="js/bootstrap.min.js"></script>
+        </div>
+    </div>
+
+
+</div>
+
+<!--<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!--<script src="../js/bootstrap-datepicker.js"></script>-->
+
+<script src="js/functions.js"></script>
+
 </body>
+
+
+
+<script src="js/control.js"></script>
 </html>
--- management/templateManagementForms.php
+++ management/templateManagementForms.php
@@ -5,9 +5,9 @@ ini_set('log_errors', 1);
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 ?>
 <?php
-include("/lib/config.php");
-require("/lib/sqlQueries.php");
-require("/lib/studenFormValid.php");
+include("../lib/config.php");
+require("../lib/sqlQueries.php");
+require("../lib/studenFormValid.php");
 ?>
 <!DOCTYPE html>
 <html lang="en" xmlns:width="http://www.w3.org/1999/xhtml">
@@ -19,53 +19,53 @@ require("/lib/studenFormValid.php");
     <title>Comp 335</title>
 
     <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/basic-template.css" rel="stylesheet" />
 
 
 
-    <link href="css/general.css" rel="stylesheet" type="text/css">
-    <link href="css/navColor.css" rel="stylesheet" type="text/css">
+    <link href="../css/general.css" rel="stylesheet" type="text/css">
+    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
     <!--    <link href="../css/datepicker.css" rel="stylesheet" type="text/css">-->
 
     <!-- BootstrapValidator CSS -->
-    <link href="css/bootstrapValidator.min.css" rel="stylesheet"/>
+    <link href="../css/bootstrapValidator.min.css" rel="stylesheet"/>
 
 
-    <link href="css/general.css" rel="stylesheet" type="text/css">
-    <link href="css/navColor.css" rel="stylesheet" type="text/css">
+    <link href="../css/general.css" rel="stylesheet" type="text/css">
+    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
 
     <!-- jQuery and Bootstrap JS -->
-    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="../js/jquery.min.js" type="text/javascript"></script>
 
 
     <!-- BootstrapValidator -->
-    <script src="js/bootstrapValidator.min.js" type="text/javascript"></script>
+    <script src="../js/bootstrapValidator.min.js" type="text/javascript"></script>
 
     <!-- Include Bootstrap Datepicker -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css" />
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
 
-    <script src="js/bootstrap-datepicker.min.js"></script>
-    <script src="js/formValidation.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap-datepicker.min.js"></script>
+    <script src="../js/formValidation.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.min.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="../js/html5shiv.min.js"></script>
+    <script src="../js/respond.min.js"></script>
     <![endif]-->
-    <script src="js/control.js"></script>
+    <script src="../js/control.js"></script>
 
 </head>
 <body>
 <div class="container-fluid bg-info">
     <div id="navigation">
         <div class="row">
-            <?php require("navigation.php"); ?>
+            <?php require("navigationManagement.php"); ?>
         </div>
     </div>
     <div class="panel panel-default  col-lg-6 col-lg-offset-1" style="width: 80%; height: 120%">
-        <div class="panel-heading h2 text-center">TEMPLATE</div>
+        <div class="panel-heading h2 text-center">TEMPLATE MANAGEMENT FORMS</div>
         <div class="panel-body">
 
         </div>
@@ -77,11 +77,11 @@ require("/lib/studenFormValid.php");
 <!--<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <!--<script src="../js/bootstrap-datepicker.js"></script>-->
 
-<script src="js/functions.js"></script>
+<script src="../js/functions.js"></script>
 
 </body>
 
 
 
-<script src="js/control.js"></script>
+<script src="../js/control.js"></script>
 </html>
