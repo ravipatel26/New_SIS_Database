f4f34602ddfe8fed1bbdc394b4b42ffdb94e483e
Alain <alainf971@hotmail.com>
2015-11-16 12:02:02 -0500

    navigation bar for queries

--- navigation.php
+++ navigation.php
@@ -11,13 +11,12 @@ $html = '<nav class="navbar navbar-default" role="navigation">
                </div>
                <div class="collapse navbar-collapse" id="navigationBar-collapse">
                   <ul class="nav navbar-nav">
-				  	  <li><a href="#">page1</a></li>
-                  	  <li><a href="#">page2</a></li>
-                      <li><a href="#">page3</a></li>
-					  <li><a href="#">page4</a></li>
-                      <li><a href="#">page5</a></li>
-                      <li><a href="#">page6</a></li>
-					  <li><a href="#">page7</a></li>
+				  	  <li><a href="#">Course Info</a></li>
+                  	  <li><a href="#">Student Info</a></li>
+                      <li><a href="#">Class List</a></li>
+					  <li><a href="#">Student Support</a></li>
+                      <li><a href="#">Grades Info</a></li>
+                      <li><a href="#">Committees Info</a></li>
 					  <li><a href="admin/adminLogin.php">Admin</a></li>
                                         </ul>
                </div>
