77ca01b505e0f4d0f5bc62c5010aebbba8a8e400
Alain <alainf971@hotmail.com>
2015-11-16 17:41:52 -0500

    formatted rest of files

--- management/courseInformationForm.php
+++ management/courseInformationForm.php
@@ -10,52 +10,9 @@ require("../lib/sqlQueries.php");
 ?>
 <!DOCTYPE html>
 <html lang="en" xmlns:width="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Comp 335</title>
+<?php require("headerManagement.php");?>
 
-    <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" />
-    <link href="css/basic-template.css" rel="stylesheet" />
-
-
-
-    <link href="../css/general.css" rel="stylesheet" type="text/css">
-    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
-    <!--    <link href="../css/datepicker.css" rel="stylesheet" type="text/css">-->
-
-    <!-- BootstrapValidator CSS -->
-    <link href="../css/bootstrapValidator.min.css" rel="stylesheet"/>
-
-
-    <link href="../css/general.css" rel="stylesheet" type="text/css">
-    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
-
-    <!-- jQuery and Bootstrap JS -->
-    <script src="../js/jquery.min.js" type="text/javascript"></script>
-
-
-    <!-- BootstrapValidator -->
-    <script src="../js/bootstrapValidator.min.js" type="text/javascript"></script>
-
-    <!-- Include Bootstrap Datepicker -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css" />
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
-
-    <script src="../js/bootstrap-datepicker.min.js"></script>
-    <script src="../js/formValidation.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="../js/html5shiv.min.js"></script>
-    <script src="../js/respond.min.js"></script>
-    <![endif]-->
-    <script src="../js/control.js"></script>
-
-</head>
 <body>
 <div class="container-fluid bg-info" style="height: 1500px">
     <div id="navigation">
--- management/studentForm.php
+++ management/studentForm.php
@@ -12,60 +12,9 @@ require("../lib/newStudentProcess.php");
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Comp 335</title>
+<?php require("headerManagement.php");?>
 
-    <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../css/basic-template.css" rel="stylesheet" />
-
-
-
-    <link href="../css/general.css" rel="stylesheet" type="text/css">
-    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
-    <link href="../css/datepicker.css" rel="stylesheet" type="text/css">
-
-    <!-- BootstrapValidator CSS -->
-    <link href="../css/bootstrapValidator.min.css" rel="stylesheet"/>
-
-
-    <link href="../css/general.css" rel="stylesheet" type="text/css">
-    <link href="../css/navColor.css" rel="stylesheet" type="text/css">
-
-
-    <!-- jQuery and Bootstrap JS -->
-    <script src="../js/jquery.min.js"></script>
-
-
-
-    <!-- BootstrapValidator -->
-    <script src="../js/bootstrapValidator.min.js"></script>
-<!--    <script src="../js/formValidation.min.js"></script>-->
-
-
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Include Bootstrap Datepicker -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css" />
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
-
-
-    <script src="../js/bootstrap-datepicker.min.js"></script>
-
-
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="../js/html5shiv.min.js"></script>
-    <script src="../js/respond.min.js"></script>
-    <![endif]-->
-
-
-</head>
 <body onload="document.getElementById('displayPosition').style.display = 'none';document.getElementById('displayLevel').style.display = 'none';document.getElementById('displaySummer').style.display = 'none';">
 
 <div class="container-fluid bg-info" style="height: 1500px">
