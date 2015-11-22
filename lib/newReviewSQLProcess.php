1468b004869c672ed4106d3240d94177173537b0
Alain <alainf971@hotmail.com>
2015-11-22 00:05:53 -0500

    modified review form

--- lib/newReviewSQLProcess.php
+++ lib/newReviewSQLProcess.php
@@ -9,19 +9,13 @@ ob_start();
 
 <?php
 
-$profFirstName = $profLastName = $editorialBoardName = $journalName = $journalYear = '';
+$editorialBoardName = $journalName = $journalYear = '';
 
 echo print_r($_POST);
 
 if (isset($_POST['submit'])) {
 
 
-    if (isset($_POST['profFirstName'])) {
-        $profFirstName = $_POST['profFirstName'];
-    }
-    if (isset($_POST['profLastName'])) {
-        $profLastName = $_POST['profLastName'];
-    }
     if (isset($_POST['editorialBoardName'])) {
         $editorialBoardName = $_POST['editorialBoardName'];
     }
